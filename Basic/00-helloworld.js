626ad0e8a1b0be1a7485ac09a1107f5e40b330bd
dtuquer-2025318 <dtuquer-2025318@kinal.edu.gt>
2026-04-22 08:03:08 -0600

    carpeta para archivos de JavaScript

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,37 @@
+//Esto es un comentario simple
+
+/*
+Esto es un
+comentario de
+varias lineas en JavaScript
+*/
+
+//console.log -> Se utiliza para imprimir mensajes de texto.
+console.log("!Hola, JavaScript!")
+
+//No importan las comillas, si son simples de igual forma se imprime el mensaje
+console.log('!Hola, JavaScript!')
+console.log(`!Hola, JavaScript!`)
+
+//imprime el numero 5.
+console.log("5")
+//imprime el numero 5.
+console.log(5)
+//imprime la suma de los dos numeros.
+//El signo +, funciona para sumar los numeros.
+console.log(5 + 2)
+//imprime la resta de los dos numeros.
+//El signo -, funciona para a restar los numeros.
+console.log(5 - 2)
+//imprime la multiplicacion de los dos numeros.
+//El signo *, funciona para multiplicar los numeros
+console.log(5 * 2)
+//imprime la divicion de los dos numeros.
+//El signo /, funciona para dividir los numeros
+console.log(5 / 2)
+//imprime el porcentaje del numero.
+//El signo %, funciona para el porcentaje del numero
+console.log(5 % 2)
+//imprime elevado el numero.
+//El signo **, funciona para elevar un numero
+console.log(5 ** 3)
